9321e16cf400ba0b3e89887116ff9a55a2dc1d3b
Mouse <mouse0w0@qq.com>
2019-05-04 18:54:42 +0800

    Update

---
+++ src/main/java/com/github/unknowndomaingame/developmentkit/DevelopmentKit.java
@@ -0,0 +1,14 @@
+package com.github.unknowndomaingame.developmentkit;
+
+import unknowndomain.engine.event.Listener;
+import unknowndomain.engine.event.game.GameEvent;
+import unknowndomain.engine.mod.Mod;
+
+@Mod("developmentkit:1.0.0")
+public class DevelopmentKit {
+
+    @Listener
+    public void onReady(GameEvent.Ready event) {
+        System.out.println("Hello UDEngine");
+    }
+}
